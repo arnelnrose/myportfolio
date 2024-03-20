fcd76c2484da7656f492e67d9216cc7a42c02d2f
Esor <155610717+esorlenra@users.noreply.github.com>
2024-03-21 00:04:27 +0800

    update porfolio

--- script.js
+++ script.js
@@ -1,50 +1,56 @@
-// Toggle icon and navbar
-document.querySelector("#menu-icon").onclick = () => {
-  document.querySelector("#menu-icon").classList.toggle("bx-x");
-  document.querySelector(".navbar").classList.toggle("active");
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 };
 
 // Scroll Sections Active
-window.onscroll = () => {
-  let navLinks = document.querySelectorAll("header nav a");
-  let top = window.scrollY;
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('header nav a');
 
-  document.querySelectorAll("section").forEach((sec) => {
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+  window.onscroll = () =>{
+    sections.forEach(sec =>{
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((link) => {
-        link.classList.remove("active");
-      });
-      document
-        .querySelector("header nav a[href*=" + id + "]")
-        .classList.add("active");
-    }
-  });
-
+      if(top >= offset && top < offset + height){
+        navLinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
+        });
+      };
+    });
+  };
+    
   // Sticky Navbar
-  document.querySelector("header").classList.toggle("sticky", top > 100);
+ let header = document.querySelector('header');
+ header.classList.toggle('sticky', window.scrollY > 100);
 
-  // Remove toggle icon and navbar when click navbar link (scroll)
-  document.querySelector("#menu-icon").classList.remove("bx-x");
-  document.querySelector(".navbar").classList.remove("active");
-};
+// Remove toggle icon navbar
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 
 // Scroll Reveal
-ScrollReveal({ reset: true, distance: "80px", duration: 2000, delay: 200 });
+ScrollReveal({ 
+  reset: true, 
+  distance: '80px', 
+  duration: 2000, 
+  delay: 200
+});
 
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
-ScrollReveal().reveal(".home-img, .portfolio-box, .contact form", {
-  origin: "bottom",
-});
+ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact form", {origin: "bottom",});
 ScrollReveal().reveal(".home-content h1, .about img", { origin: "left" });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 // Type JS
-new Typed(".multi-text", {
-  strings: ["IT Field Engineer", "SO Branch Head"],
+const typed = new Typed(".multi-text", {
+  strings: ["IT Field Engineer", "Branch Head",],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,

25d25617553119fb1bc89b9007636696b77653c6
Esor <155610717+esorlenra@users.noreply.github.com>
2024-03-20 18:46:32 +0800

    Delete StickyNavbar directory

--- StickyNavbar/script.js
+++
@@ -1,9 +0,0 @@
-/* Background */
-window.onload = function() {Particles.init ({selector: '.background'});
-};
-
-/* Sticky Header */
-window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", windows.scrollY > 0);
-  });
